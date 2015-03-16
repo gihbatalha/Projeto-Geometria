c43719b50456fea30c6e6dfa6a0d52815cca492a
Giovanna Batalha <giovanna.batalha03@gmail.com>
2015-03-15 22:24:21 -0300

    Adicionando a classe Ponto.java

---
+++ src/Classes/Ponto.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import java.awt.*;
+public class Ponto {
+	private int x,y;
+	private Color cor;
+	
+	public Ponto(int cX, int cY, Color qualCor)
+	{
+		x = cX;
+	  	y = cY;
+	  	cor = qualCor;
+	}
+
+}
